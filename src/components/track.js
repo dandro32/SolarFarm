402767733417376a6672e8c8f6579e1066df6c5a
Daniel <daniel.drozdzel@vattenfall.com>
2021-01-04 09:27:27 +0100

    Flatilist enhancements

--- src/components/track.js
+++ src/components/track.js
@@ -1,29 +1,44 @@
 import React, {useContext, useEffect} from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, Text, View, Pressable, LogBox} from 'react-native';
 import AnimatedEllipsis from 'react-native-animated-ellipsis';
 
 import styled from 'styled-components/native';
 import {AppContext} from '../state/context';
 
 const Track = () => {
+
+  useEffect(() => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver` was not specified.']);
+    return () => {};
+  }, []);
+
   const {items} = useContext(AppContext);
+  const goToDetails = (id) => {
+    console.log({id});
+  };
 
   return (
     <TrackView>
       <IsTrackingWrapper>
-        <IsTrackingText>
-          Tracking
-          <AnimatedEllipsis
-            style={{
-              color: '#00e0ff',
-              fontSize: 40,
-            }}
-          />
-        </IsTrackingText>
+        <IsTrackingText>Tracking</IsTrackingText>
+        <AnimatedEllipsis
+          style={{
+            color: '#00e0ff',
+            fontSize: 40,
+          }}
+        />
       </IsTrackingWrapper>
       <FlatListWrapper
         data={items}
-        renderItem={({item}) => <SolarItem>{item.name}</SolarItem>}
+        renderItem={({item}) => (
+          <Pressable onPress={() => goToDetails(item.id
+          )}>
+            <SolarItem>
+              <SolarItemText>{item.name}</SolarItemText>
+              <SolarItemText>{item.distance}m</SolarItemText>
+            </SolarItem>
+          </Pressable>
+        )}
       />
     </TrackView>
   );
@@ -34,6 +49,8 @@ export const TrackView = styled.View`
 `;
 
 export const IsTrackingWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   height: 100px;
@@ -41,7 +58,18 @@ export const IsTrackingWrapper = styled.View`
 
 export const FlatListWrapper = styled.FlatList``;
 
-export const SolarItem = styled.Text`
+export const SolarItem = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+  border-width: 1px 
+  border-top-color:#00e0ff;
+`;
+
+export const SolarItemText = styled.Text`
+  font-size: 18;
   color: #00e0ff;
 `;
 
--- src/state/context.js
+++ src/state/context.js
@@ -5,6 +5,7 @@ export const AppContext = React.createContext({});
 const DUMMY_SOLAR_ITEMS = [...Array(30).keys()].map((nr) => ({
   id: `id_${nr}`,
   name: `Solar Panel: ${nr}`,
+  distance: (Math.random() * 10).toFixed(1),
   details: [],
 }));
 
